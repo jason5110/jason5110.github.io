51d5ba93928efcbea93e0442749395121942154b
jason5110 <jason51102000@gmail.com>
2018-01-25 16:13:38 +0800

    add ToDoApp/index.js

--- js/Test/index.js
+++ js/Test/index.js
@@ -5,11 +5,6 @@ var html = '<h1>Hello, world!</h1>'
 class Test extends React.Component {
     render() {
       return React.createElement("h1", {dangerouslySetInnerHTML: {__html: html}})
-        // return (
-        //   <div>
-        //     <p>test</p>
-        //   </div>
-        // );
     }
 }
 
