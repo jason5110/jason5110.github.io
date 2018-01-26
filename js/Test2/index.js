66bc90e2714b1962390b096dfaf6d39c14a3d8b3
jason5110 <jason51102000@gmail.com>
2018-01-26 15:53:05 +0800

    add test2

---
+++ js/Test2/index.js
@@ -0,0 +1,15 @@
+
+
+class Test2 extends React.Component {
+    render() {
+      return (
+        <div>
+          <p>
+            Hello, world!
+          </p>
+        </div>
+      )
+    }
+}
+
+ReactDOM.render(React.createElement(Test2), document.getElementById('test2'))
