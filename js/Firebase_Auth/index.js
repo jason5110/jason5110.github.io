a95cd0ff3c38250bbe3488bd344f87d1ed2ce992
jason5110 <jason51102000@gmail.com>
2018-01-26 17:49:51 +0800

    add firebase_auth

---
+++ js/Firebase_Auth/index.js
@@ -0,0 +1,56 @@
+(function() {
+	var config = {
+        apiKey: "AIzaSyCIt-SXHFUBMsfSyACYNC6h7KbHrmT53Uc",
+        authDomain: "eatwhat-6151420538.firebaseapp.com",
+        databaseURL: "https://eatwhat-6151420538.firebaseio.com",
+        // projectId: "eatwhat-6151420538",
+        // storageBucket: "eatwhat-6151420538.appspot.com",
+        // messagingSenderId: "573626268392"
+    };
+
+
+    if (!firebase.apps.length) {
+	    firebase.initializeApp(config);
+	}
+    // firebase.initializeApp(config);
+
+    const txtEmail = document.getElementById('txtEmail');
+    const txtPassword = document.getElementById('txtPassword');
+    const btnLogin = document.getElementById('btnLogin');
+    const btnLogout = document.getElementById('btnLogout');
+
+
+
+    btnLogin.addEventListener('click', e=> {
+    	const email = txtEmail.value;
+    	const pass = txtPassword.value;
+    	const auth = firebase.auth();
+
+    	console.log('btnLogin email: '+JSON.stringify(email)+', pass: '+JSON.stringify(pass))
+
+    	const promise = auth.signInWithEmailAndPassword(email, pass)
+
+
+    	promise.catch(e => console.log(e.message));
+    })
+
+    btnLogout.addEventListener('click', e => {
+    	firebase.auth().signOut();
+    })
+
+
+    firebase.auth().onAuthStateChanged(firebaseUser => {
+    	if (firebaseUser) {
+    		console.log(firebaseUser)
+    		btnLogout.classList.remove('hide');
+    		btnLogin.classList.add('hide');
+    	} 
+    	else{
+    		console.log('not logged in')
+    		btnLogout.classList.add('hide');
+    		btnLogin.classList.remove('hide');
+    	}
+    })
+
+
+}());
